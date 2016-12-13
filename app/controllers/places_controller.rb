90f24039ec5a0eed11b63a37edc6cf4c0dc82396
Alyson Stiffel <alysonstiffel@gmail.com>
2016-12-13 23:53:25 +0000

    Fixed permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,9 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
