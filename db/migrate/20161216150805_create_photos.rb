399acca5be6f414bfbc10dae3246e93f402d3bf5
Alyson Stiffel <alysonstiffel@gmail.com>
2016-12-18 00:12:49 +0000

    Fixed photo issues

--- db/migrate/20161216150805_create_photos.rb
+++ db/migrate/20161216150805_create_photos.rb
@@ -2,6 +2,7 @@ class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
       t.text :caption
+      t.integer :user_id
       t.integer :place_id
 
       t.timestamps
