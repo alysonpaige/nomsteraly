4ab5b03a7bdfecf7522fedb27391732195afcd32
Alyson Stiffel <alysonstiffel@gmail.com>
2016-12-20 02:36:37 +0000

    Mobile, validation, and other tweaks

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,12 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params)
-    redirect_to place_path(@place)
+    if @place.valid?
+      @place.photos.create(photo_params)
+      redirect_to place_path(@place)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :place
   mount_uploader :picture, PictureUploader
+  validates :caption, presence: true, length: { minimum: 4 }
 end
